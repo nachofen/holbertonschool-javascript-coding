2063f8eaab37fa4e3a22a774e0aee37f37d3a882
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-24 15:22:59 -0700

    corrected folder

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+//[2] will be the route of the file
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    //[2] will be the route of the file
   if (err) {
     console.log(err);
     return;
