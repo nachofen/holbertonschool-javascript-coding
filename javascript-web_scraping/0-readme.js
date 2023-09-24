4ba58529b2bf0086423507e8dd0e8435cae68314
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-24 15:24:16 -0700

    semistandard validation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-//[2] will be the route of the file
+// [2] will be the route of the file
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
