507ce2cefa7ef8222aadacfbdacc099d38a663ba
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-24 16:30:39 -0700

    task2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
     if (error) {
-      console.error(error);
-      return;
+        console.error(error);
+        return;
     }
   
     console.log(`code: ${response.statusCode}`);
