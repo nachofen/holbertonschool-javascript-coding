b7aeb301b55e2ca801d60e22c7daa2845752b652
nachofen <124267664+nachofen@users.noreply.github.com>
2023-09-24 15:37:11 -0700

    semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 const fPath = process.argv[2];
-const toWrite = process.argv[3]
+const toWrite = process.argv[3];
 
 // [2] will be the route of the file, [3] the text to write
 fs.writeFile(fPath, toWrite, 'utf8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
 });
